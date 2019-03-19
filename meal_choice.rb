c17c3bfc2deb201d8ddd5c89940311a1eff486ee
oneilld136 <oneilld136@gmail.com>
2019-03-19 15:21:09 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,14 +1,6 @@
 def meal_choice(choice = "meat")
-  return choice 
+  choice 
 end
-
-#end
-#def meal_choice
-#  meal_choice="vegan"
-#end
-
-
-
 
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
